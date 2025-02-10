5b2de5ad13819035f72a46e0e56457805c453d27
Rémi Bastide <Remi.Bastide@irit.fr>
2025-02-10 13:28:19 +0100

    Correction JPA

--- src/main/java/ssii/entity/Personne.java
+++ src/main/java/ssii/entity/Personne.java
@@ -1,25 +1,56 @@
 package ssii.entity;
 
 import jakarta.persistence.*;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@RequiredArgsConstructor // lombok, pour générer un constructeur avec les champs @NonNull
+@RequiredArgsConstructor
 @ToString
 public class Personne {
 
     @Id
+    @Setter(AccessLevel.NONE) // Ne génère pas de setter pour cet attribut
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer matricule;
 
-    @NotBlank
-    @NonNull // lombok
+    @Column(nullable = false)
+    @NonNull
     private String nom;
 
+    @Column(nullable = false)
+    @NonNull
+    private String prenom;
+
+    @Column(nullable = false)
+    @NonNull
+    private String poste;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "personne", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Participation> affectations = new ArrayList<>();
+
+    @ToString.Exclude
+    @ManyToOne
+    private Personne superieur;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "superieur", orphanRemoval = true)
+    private List<Personne> subordonnes = new ArrayList<>();
+
+    /**
+     * Ajoute une participation à un projet pour cette personne
+     * @param affectation Le projet auquel la personne participe
+     * @param role Le rôle de la personne dans le projet
+     * @param pourcentage Le pourcentage de temps que la personne consacre au projet
+     */
+    public void addParticipation(Projet affectation, String role, float pourcentage) {
+        var participation = new Participation(role, pourcentage, affectation, this);
+        affectations.add(participation);
+    }
 }
